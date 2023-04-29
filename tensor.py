f603b7bec84da5713964bfb58472d889ff30ec3a
woo <rich72@hanamil.net>
2023-04-30 00:20:33 +0900

    first commit

--- tensor.py
+++ tensor.py
@@ -1,1 +1,20 @@
-sdafsdf
+# def sum1(a,b):
+#     return a+b
+
+# a = sum1(3,2)
+# print(a)
+
+
+# from math import pi
+
+# def area_circle(radius, n):
+#     return round(pi * radius ** 2, n)
+
+# print(area_circle(1))
+# print(area_circle(2))
+# print(area_circle(3))
+
+def open_account():
+    print("새로운 계좌가 생성되었습니다.")
+
+open_account()
